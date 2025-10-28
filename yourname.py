fb091367f8e6dd4ae76da30c6a8478982c8a5739
rubelali5659 <rubelali5659@gmail.com>
2025-10-28 18:44:42 +0600

    new file

---
+++ yourname.py
@@ -0,0 +1,6 @@
+# Let's try the welocme name again
+ali = input('Who are you: ')
+am = input('where are you from: ')
+x = input('whats your marital status: ')
+y = input('whose your spouse: ')
+print("So you are",ali,"from",am,"and you are",x,"your wife is",y)
