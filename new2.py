5568863bbba390b01a3083460997160c2c458101
rubelali5659 <rubelali5659@gmail.com>
2025-11-05 09:08:59 -0500

    new code

---
+++ new2.py
@@ -0,0 +1,16 @@
+sh = input("Enter hour: ")
+sr = input("Enter rate: ")
+xh = float(sh)
+xr = float(sr)
+if xh>40:
+    # print("Overtime")
+    reg = xr*40
+    otp = (xh-40)*(xr*10.5)
+    # print(reg,otp)
+    xp = reg+otp
+    print(f"Pay:,{xp:.2f}")
+else:
+    # print("Regular")
+    xp = xh*xr
+    print(f"Pay:,{xp:.2f}")
+    
