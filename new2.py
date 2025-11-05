37d4cfe032518b502fa27c24a6580e7a318f908e
rubelali5659 <rubelali5659@gmail.com>
2025-11-05 09:30:12 -0500

    new code

--- new2.py
+++ new2.py
@@ -1,15 +1,10 @@
 sh = input("Enter hour: ")
 sr = input("Enter rate: ")
-xh = float(sh)
-xr = float(sr)
-if xh>40:
-    # print("Overtime")
-    reg = xr*40
-    otp = (xh-40)*(xr*1.5)
-    # print(reg,otp)
-    xp = reg+otp
-    print(f"Pay:{xp:.2f}")
-else:
-    # print("Regular")
-    xp = xh*xr
-    print(f"Pay:{xp:.2f}")
+try:
+    xh = float(sh)
+    xr = float(sr)
+except:
+    print("Error: put numeric number")
+    quit()
+xp = xh*xr
+print(f"Total Pay:{xp:.2f}")
