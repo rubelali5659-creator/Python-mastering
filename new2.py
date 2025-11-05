5078c1f06a2e9c2461c08b23c651f45bcab71d2f
rubelali5659 <rubelali5659@gmail.com>
2025-11-05 09:10:48 -0500

    new change

--- new2.py
+++ new2.py
@@ -8,9 +8,8 @@ if xh>40:
     otp = (xh-40)*(xr*10.5)
     # print(reg,otp)
     xp = reg+otp
-    print(f"Pay:,{xp:.2f}")
+    print(f"Pay:{xp:.2f}")
 else:
     # print("Regular")
     xp = xh*xr
-    print(f"Pay:,{xp:.2f}")
-    
+    print(f"Pay:{xp:.2f}")
