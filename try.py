d0c0abad1f511e24cecbcb3a87a2a45405b3e069
rubelali5659 <rubelali5659@gmail.com>
2025-11-07 09:46:52 -0500

    new code practise

---
+++ try.py
@@ -0,0 +1,16 @@
+def computepay(hours,rate):
+    #print("Incomputepay", hours, rate)
+    if hours>40:
+        reg = 40*rate
+        otp = (hours-40)*(rate*1.5)
+        pay = reg + otp
+    else:
+        pay = hours*rate
+    #print("Returning", pay)
+    return pay
+sh = input("Enter hour: ")
+sr = input("Enter rate: ")
+fh = float(sh)
+fr = float(sr)
+xp = computepay(fh,fr)
+print(f"Pay {xp:.2f}")
