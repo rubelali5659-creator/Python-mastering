cc22d334a24a9ff7dd9eafa403ca376cdb76e1df
rubelali5659 <rubelali5659@gmail.com>
2025-11-07 12:39:08 -0500

    grading

---
+++ ex32.py
@@ -0,0 +1,20 @@
+score_input =input("Enter a score between 0.0 and 1.0: ")
+try:
+    score = float(score_input)
+    if score < 0.0 or score > 1.0:
+        print("Error: out of range")
+    else:
+        if score >= 0.9:
+            Grade = "A"
+        elif score >= 0.8:
+            Grade = "B"
+        elif score >= 0.7:
+            Grade = "C"
+        elif score >= 0.6:
+            Grade = "D"
+        elif score < 0.6:
+            Grade = "F"
+    print(Grade)
+except ValueError:
+    print("Error!")
+
