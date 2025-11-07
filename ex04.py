b9f9cf04d2699da550c296dbb18933ce9105e836
rubelali5659 <rubelali5659@gmail.com>
2025-11-07 09:03:26 -0500

    chnage code

--- ex04.py
+++ ex04.py
@@ -1,16 +1,15 @@
 def computepay(hours, rate):
     print("In computepay", hours, rate)
-    sh = input("Enter hour: ")
-    sr = input("Enter rate: ")
-    xh = float(sh)
-    xr = float(sr)
-    computepay(xh,xr)
-    if xh>40:
-        #print("Overtime")
-        reg = 40*xr
-        otp = (xh-40)*(xr*1.5)
-        xp = reg + otp
-    else:
-        #print("Regular")
-        xp = xh*xr
-    print(f"Pay:{xp:.2f}")
+
+sh = input("Enter hour: ")
+sr = input("Enter rate: ")
+fh = float(sh)
+fr = float(sr)
+computepay(fh,fr)
+if fh>40:
+    reg = 40*fr
+    otp = (fh-40)*(fr*1.5)
+    xp = reg + otp
+else:
+    xp = fh*fr
+print(f"Pay:{xp:.2f}")
